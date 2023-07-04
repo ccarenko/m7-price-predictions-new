a461e0124e34844e6d7f32be17b7120fe43fb4ce
Samuel Hinton <samuelreay@gmail.com>
2023-07-04 14:19:07 +1000

    Updating template to have so many rules

--- docs/src/package/example.py
+++ docs/src/package/example.py
@@ -1,2 +1,15 @@
+import os
+import sys
+from datetime import datetime as dt
+from datetime import timezone as tz
+
+
 def add(a: int, b: int) -> int:
     return a + b
+
+
+class GoodName:
+    def __init__(self):
+        self.something = os.environ.get("SOMETHING", "default")
+        self.something2 = sys.argv
+        self.now = dt.now(tz=tz.utc)
